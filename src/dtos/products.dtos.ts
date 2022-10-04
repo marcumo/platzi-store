3fec4a850246d110f274f5ea8ac65e70c3defb0a
marcumo <macuevam@gmail.com>
2022-10-04 00:40:28 -0500

    whitelist activated in ValidationPipe

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,16 +1,29 @@
+import { IsNotEmpty, IsNumber, IsPositive, IsString, IsUrl } from "class-validator";
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stok: number;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly image?: string;
-  readonly stok?: number;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
 
